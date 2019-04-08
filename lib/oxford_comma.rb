0d76d53008412e4984ab9dd557766c4060738190
Alexander Hare <alexanderhare14@gmail.com>
2019-04-08 23:53:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,13 @@
 # end
 
 def oxford_comma(array)
-  array_1 = array.slice(0,array.length - 1)
-  array_2 = array_1.join(', ')
-  array_2 + ", and #{array[-1]}"
+  if array.length == 1
+    array.join(', ')
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else =
+    array_1 = array.slice(0,array.length - 1)
+    array_2 = array_1.join(', ')
+    array_2 + ", and #{array[-1]}"
+  end
 end
